aaafce4e87b674f85dfeac9be9f314a38b4aabaa
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-10-29 21:24:39 +0530

    todo create and list completed

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="w-full flex items-center justify-center p-5">
         <div class="w-1/3">
-            <h1 class="text-2xl mt-5 mb-24 w-full text-center">Todo App</h1>
+            <div class="w-full flex flex-row justify-between mt-5 mb-24">
+                <h1 class="text-2xl">Todo App</h1>
+                <p>{{ $pb.authStore?.model?.name ?? "Anonymous" }}</p>
+            </div>
             <!-- Input -->
             <div class="flex gap-2 justify-center">
-                <InputText type="text" size="normal" class="w-full" placeholder="Enter Todo" v-model="draftTodo" />
-                <LButton @click="addTodo" size="normal" class="min-w-fit"><i class="pi pi-plus mr-1"
-                        style="font-size: 1rem"></i>Add
+                <InputText type="text" size="normal" class="w-full" placeholder="Enter Todo" v-model="draftTodo"
+                    :readonly="isSubmitting" />
+                <LButton @click="addTodo" size="normal" class="min-w-fit" :loading="isSubmitting"><i
+                        class="pi pi-plus mr-1" style="font-size: 1rem"></i>Add
                     New
                     Todo</LButton>
             </div>
@@ -33,35 +37,43 @@ export default {
     data() {
         return {
             draftTodo: '',
-            data: [
-                {
-                    id: 1,
-                    title: 'Todo 1',
-                    completed: false,
-                },
-                {
-                    id: 2,
-                    title: 'Todo 2',
-                    completed: true,
-                },
-                {
-                    id: 3,
-                    title: 'Todo 3',
-                    completed: false,
-                },
-            ],
+            isSubmitting: false,
+            data: [],
         }
     },
+    mounted() {
+        this.loadData();
+    },
     methods: {
-        addTodo() {
-            // alert(this.draftTodo)
-            this.data.push({
-                id: this.data.length + 1,
-                title: this.draftTodo,
-                completed: false,
-            })
-            this.draftTodo = ''
+        async addTodo() {
+            if (!this.draftTodo) {
+                alert("Please enter a valid title")
+                return
+            }
+            this.isSubmitting = true;
+            try {
+                await this.$pb.collection('todos').create({
+                    "title": this.draftTodo,
+                    "completed": false,
+                    "user": this.$pb.authStore.model.id
+                });
+                this.draftTodo = '';
+            } catch (error) {
+                alert(error.response?.message ?? "Unknown error");
+            } finally {
+                this.isSubmitting = false;
+            }
         },
+        async loadData() {
+            try {
+                const records = await this.$pb.collection('todos').getFullList({
+                    sort: '-created',
+                });
+                this.data = records;
+            } catch (error) {
+                alert(error.response?.message ?? "Unknown error");
+            }
+        }
     },
 }
 </script>
