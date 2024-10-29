7cb8a2e84d9eda74110dc6d6952eb031c0d0b966
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-10-29 21:07:40 +0530

    login signup func completed

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('https://test1.ip-167-235-30-42.swiftwave.xyz');
 
 const app = createApp(App)
 
@@ -16,5 +19,6 @@ app.use(PrimeVue, {
     }
 });
 
+app.config.globalProperties.$pb = pb
 
 app.mount('#app')
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -5,7 +5,7 @@
             <p class="text-2xl font-medium mb-10">Log In</p>
             <InputText type="text" size="normal" class="w-full mb-2" placeholder="Email ID" v-model="email" />
             <InputText type="text" size="normal" class="w-full mb-4" placeholder="Password" v-model="password" />
-            <LButton size="normal" class="w-full">Log in</LButton>
+            <LButton size="normal" class="w-full" @click="login">Log in</LButton>
             <p class="mt-5">Don't have an account?
                 <span class="text-primary cursor-pointer" @click="isLoginFormActive = false">Sign Up</span>
             </p>
@@ -13,9 +13,10 @@
         <!-- signup -->
         <div class="border-2 border-primary rounded-lg p-5 w-1/4 flex flex-col items-center" v-else>
             <p class="text-2xl font-medium mb-10">Sign Up</p>
+            <InputText type="text" size="normal" class="w-full mb-2" placeholder="Name" v-model="name" />
             <InputText type="text" size="normal" class="w-full mb-2" placeholder="Email ID" v-model="email" />
             <InputText type="text" size="normal" class="w-full mb-4" placeholder="Password" v-model="password" />
-            <LButton size="normal" class="w-full">Create New Account</LButton>
+            <LButton size="normal" class="w-full" @click="signup" :loading="isLoading">Create New Account</LButton>
             <p class="mt-5">Already have an account?
                 <span class="text-primary cursor-pointer" @click="isLoginFormActive = true">Log In</span>
             </p>
@@ -34,9 +35,52 @@ export default {
     },
     data() {
         return {
+            name: "",
             email: "",
             password: "",
-            isLoginFormActive: true
+            isLoginFormActive: true,
+            isLoading: false,
+        }
+    },
+    methods: {
+        async signup() {
+            if (!this.email || !this.password) {
+                alert("Please enter email and password")
+            }
+            const data = {
+                "email": this.email,
+                "emailVisibility": true,
+                "password": this.password,
+                "passwordConfirm": this.password,
+                "name": this.name
+            };
+            this.isLoading = true;
+
+            try {
+                await this.$pb.collection('users').create(data);
+                alert("User registration successful. Login to continue")
+                this.isLoading = false;
+                this.isLoginFormActive = true;
+            } catch (error) {
+                alert(error.response?.message ?? "Unknown error");
+                this.isLoading = false;
+            }
+        },
+        async login() {
+            if (!this.email || !this.password) {
+                alert("Please enter email and password")
+            }
+            this.isLoading = true;
+
+            try {
+                await this.$pb.collection('users').authWithPassword(this.email, this.password);
+                alert("Login successful. Welcome back")
+                this.isLoading = false;
+                this.$router.push('/');
+            } catch (error) {
+                alert(error.response?.message ?? "Unknown error");
+                this.isLoading = false;
+            }
         }
     }
 }
