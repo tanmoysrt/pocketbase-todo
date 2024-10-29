6e632643939bb012bcefd2ab237186e8acad99b2
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-10-29 19:15:13 +0530

    tailwind with ui framework dded

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
+import "./assets/style.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
 const app = createApp(App)
 
 app.use(router)
-app.use(PrimeVue);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 
 
 app.mount('#app')
